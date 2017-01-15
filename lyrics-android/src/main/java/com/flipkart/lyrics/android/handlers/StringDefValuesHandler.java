2e9f8a303d173a3b425bd54aaae46fa972904d7e
shrey-garg <shrey.garg@flipkart.com>
2017-01-15 22:02:46 +0530

    Adding license blocks

--- lyrics-android/src/main/java/com/flipkart/lyrics/android/handlers/StringDefValuesHandler.java
+++ lyrics-android/src/main/java/com/flipkart/lyrics/android/handlers/StringDefValuesHandler.java
@@ -1,14 +1,32 @@
+/*
+ * Copyright 2017 Flipkart Internet, pvt ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.flipkart.lyrics.android.handlers;
 
 import com.flipkart.lyrics.android.config.AndroidTune;
 import com.flipkart.lyrics.config.Tune;
 import com.flipkart.lyrics.model.MetaInfo;
-import com.flipkart.lyrics.model.Type;
 import com.flipkart.lyrics.model.TypeModel;
 import com.flipkart.lyrics.processor.Handler;
 import com.flipkart.lyrics.processor.instances.EnumValuesHandler;
 import com.flipkart.lyrics.sets.RuleSet;
-import com.squareup.javapoet.*;
+import com.squareup.javapoet.AnnotationSpec;
+import com.squareup.javapoet.ClassName;
+import com.squareup.javapoet.FieldSpec;
+import com.squareup.javapoet.TypeSpec;
 
 import javax.lang.model.element.Modifier;
 import java.lang.annotation.Retention;
